f36718cef32ec16fd5088bed123ad28cdf090c92
Cheonho <ghtp8571@naver.com>
2025-02-18 10:40:49 +0900

    feat: route 추가

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,25 @@ import User from 'views/User';
 import BoardDetail from 'views/Board/Detail';
 import BoardWrite from 'views/Board/Write';
 import BoardUpdate from 'views/Board/Update';
+import Container from 'layouts/Container';
+import { BOARD_DETAIL_PATH, BOARD_PATH, BOARD_UPDATE_PATH, BOARD_WRITE_PATH, MAIN_PATH, SEARCH_PATH, USER_PATH } from 'constant';
+import { AUTH_PATH } from 'constant';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/auth" element={<Authentication />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/user" element={<User />} />
-        <Route path="/board">
-          <Route path="detail/:boardNum" element={<BoardDetail />} />
-          <Route path="update/:boardNum" element={<BoardUpdate />} />
-          <Route path="write" element={<BoardWrite />} />
+        <Route element={<Container />}>
+          <Route path={MAIN_PATH()} element={<Main />} />
+          <Route path={AUTH_PATH()} element={<Authentication />} />
+          <Route path={SEARCH_PATH(`:searchWord`)} element={<Search />} />
+          <Route path={USER_PATH(`:userId`)} element={<User />} />
+          <Route path={BOARD_PATH()}>
+            <Route path={BOARD_DETAIL_PATH(`:boardNum`)} element={<BoardDetail />} />
+            <Route path={BOARD_UPDATE_PATH(`:boardNum`)} element={<BoardUpdate />} />
+            <Route path={BOARD_WRITE_PATH()} element={<BoardWrite />} />
+          </Route>
+          <Route path="*" element={<h1>404 Not Found</h1>} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/layouts/Container/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from 'layouts/Header'
+import Footer from 'layouts/Footer'
+import { Outlet, useLocation } from 'react-router-dom'
+import { AUTH_PATH } from 'constant';
+
+export default function Container() {
+  const { pathname } = useLocation();
+
+  return (
+    <>
+      {pathname}
+      <Header />
+      <Outlet />
+      {pathname !== AUTH_PATH() && <Footer />}
+    </>
+  )
+}
