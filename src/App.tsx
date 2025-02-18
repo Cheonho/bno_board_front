8f309d12027855e789cdefb73d4030c89bf2389d
Cheonho <ghtp8571@naver.com>
2025-02-18 10:15:34 +0900

    feat: views 생성 및 기본 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import './App.css';
-import BoardItem from 'components/BoardItem';
-import { boardListMock } from 'mocks';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Main from 'views/Main';
+import Authentication from 'views/Authentication';
+import Search from 'views/Search';
+import User from 'views/User';
+import BoardDetail from 'views/Board/Detail';
+import BoardWrite from 'views/Board/Write';
+import BoardUpdate from 'views/Board/Update';
 
 function App() {
   return (
-    <>
-      {boardListMock.map(boardList => <BoardItem boardListType={boardList} />)}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/auth" element={<Authentication />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/board">
+          <Route path="detail/:boardNum" element={<BoardDetail />} />
+          <Route path="update/:boardNum" element={<BoardUpdate />} />
+          <Route path="write" element={<BoardWrite />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
