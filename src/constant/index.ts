3d84957c375b56a7120646448845a63a2cea1eaa
Cheonho <ghtp8571@naver.com>
2025-02-18 13:15:23 +0900

    feat: 게시판 API 추가

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,3 +1,4 @@
+// PATH
 export const MAIN_PATH = () => '/';
 export const AUTH_PATH = () => '/auth';
 export const SEARCH_PATH = (searchWord: string) => `/search/${searchWord}`;
@@ -6,3 +7,6 @@ export const BOARD_PATH = () => '/board';
 export const BOARD_DETAIL_PATH = (boardNum: string | number) => `detail/${boardNum}`;
 export const BOARD_WRITE_PATH = () => 'write';
 export const BOARD_UPDATE_PATH = (boardNum: string | number) => `update/${boardNum}`;
+
+// URL
+export const BOARD_API_URL = () => 'http://localhost:8080/api/v1/board/';
--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -1,8 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './style.css'
+import { BOARD_API_URL } from 'constant';
+import axios from 'axios';
+import BoardItem from 'components/BoardItem';
 
 export default function Main() {
+  const category = [
+    {value:1, name:"전체"},
+    {value:2, name:"작성자"},
+    {value:3, name:"제목"},
+    {value:4, name:"내용"},
+  ]
+  const [selected, setSelected] = useState(1);
+  const [searchWord, setSearchWord] = useState("");
+  const [boardList, setBoardList] = useState([]);
+
+  const handleSelect = (event: any) => {
+    setSelected(event?.target.value)
+  }
+
+  const handleSearchWord = (event: any) => {
+    setSearchWord(event?.target.value)
+  }
+
+  const getBoardList = async () => {
+    const res = await axios.get(BOARD_API_URL()+'board-list')
+    setBoardList(res.data.boardList)
+  }
+
+  const getSearchBoardList = async () => {
+    try{
+      const res = await axios.get(BOARD_API_URL()+`search-list/${selected}/${searchWord}`)
+      const resData = res.data.boardSearchList
+      setBoardList(resData)
+    } catch (err) {
+      console.log(err)
+    }
+    
+  }
+  useEffect(() => { 
+    getBoardList();
+  },[])
+
+  useEffect(() => {
+    if(searchWord && selected){
+      getSearchBoardList();
+    } else if (searchWord === "" && selected) {
+      getBoardList();
+    }
+  },[searchWord, selected])
+
   return (
-    <div>Main</div>
+    <div>
+      <div>
+        <select onChange={handleSelect} value={selected}>
+          {category.map((item, index) => {
+            return (
+              <option value={item.value} key={index}>
+                {item.name}
+              </option>
+            )
+          })}
+        </select>
+        <input type="text" value={searchWord} onChange={handleSearchWord} />
+      </div>
+
+      {boardList.map((item) => {
+        return (
+          <BoardItem boardListType={item} />
+        )
+      })}
+    </div>
   )
 }
