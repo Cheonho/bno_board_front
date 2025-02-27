a9e61affd9e82072544e8a0bcbd010e8cc685c49
Cheonho <ghtp8571@naver.com>
2025-02-27 11:15:44 +0900

    refactor: button, input 컴포넌트 수정

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -2,11 +2,18 @@ interface ButtonProps {
   text: string;
   id?: string;
   classNames?: string;
+  type?: "button" | "submit" | "reset";
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-export default function Input({text, id, classNames,  onClick}: ButtonProps) {
+export default function Input({text, id, type, classNames,  onClick}: ButtonProps) {
   return (
-    <button id={id} className={classNames ? classNames : `btn-pr`} onClick={onClick}>{text}</button>
+    <button 
+      type={type ? type : "button"}
+      id={id} 
+      className={classNames ? classNames : `btn-pr`} 
+      onClick={onClick}>
+      {text}
+    </button>
   )
 }
--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -3,13 +3,23 @@ interface InputProps {
     id?: string;
     classNames?: string;
     value?: string;
+    disabled?: boolean;
+    placeholder?: string;
     onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export default function Input({type, id, classNames, value, onChange}: InputProps) {
+export default function Input({type, id, classNames, value, onChange, disabled, placeholder}: InputProps) {
   return (
     <div>
-      <input type={type} id={id} className={classNames} value={value} onChange={onChange} />
+      <input 
+        type={type} 
+        id={id} 
+        className={classNames} 
+        value={value} 
+        disabled={disabled} 
+        placeholder={placeholder}
+        onChange={onChange} 
+      />
     </div>
   )
 }
