80ee7358f8194139df92f46bc37a31f5aae2fb4d
Cheonho <ghtp8571@naver.com>
2025-02-27 16:26:05 +0900

    feat: 특정 url 로그인 / 로그아웃 버튼 안보이도록 수정

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -3,24 +3,30 @@ import './style.css'
 import Button from "components/common/Button"
 import useUserStore from 'stores/useUserStore'
 import { clearSession } from 'utils/Login/LoginSession'
-import { useNavigate } from 'react-router-dom'
-import { LOGIN_PATH } from 'constant'
+import { useNavigate, useLocation } from 'react-router-dom'
+import { LOGIN_PATH, MAIN_PATH, FIND_ID_PW_PATH } from 'constant';
 
 export default function Header() {
   const navigate = useNavigate();
-  const clearUser = useUserStore((state) => state.clearUser)
-  const userState = useUserStore((state) => state.user)
+  const clearUser = useUserStore((state) => state.clearUser);
+  const userState = useUserStore((state) => state.user);
+  const location = useLocation().pathname;
+  const exceptionPath = [LOGIN_PATH(), FIND_ID_PW_PATH()]
+  const buttonCheck = exceptionPath.includes(location);
 
   const loginPath = `${LOGIN_PATH()}`
 
   const logoutBtn = () => {
     clearUser()
     clearSession()
+    navigate(MAIN_PATH())
   }
 
   return (
     <div className='header-box'>
-      {userState ? <Button text={"로그아웃"} classNames='btn-pr btn-login' onClick={logoutBtn}></Button> : <Button text={"로그인"} classNames='btn-pr btn-login' onClick={() => navigate(loginPath)} />}
+      {!buttonCheck && (
+        userState ? <Button text={"로그아웃"} classNames='btn-pr btn-login' onClick={logoutBtn}></Button> : <Button text={"로그인"} classNames='btn-pr btn-login' onClick={() => navigate(loginPath)} />
+      )}
     </div>
   )
 }
